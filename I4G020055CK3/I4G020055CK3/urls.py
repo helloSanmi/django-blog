a60ca76d3d510fbca11ad95bf8e54e5ac58ef3b8
Oluwasanmi Idowu <idowuoluwasanmi@gmail.com>
2022-06-24 15:33:59 +0100

    new

--- I4G020055CK3/I4G020055CK3/urls.py
+++ I4G020055CK3/I4G020055CK3/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from argparse import Namespace
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 
 
 urlpatterns = [
